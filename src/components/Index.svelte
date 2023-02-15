b31d1d98664c549eebb47d073d68db3bf5057ecd
capricec <carstensen.caprice@gmail.com>
2023-02-15 12:29:36 -0800

    continued cleanup

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,13 +1,6 @@
 <script>
-	import { getContext } from "svelte";
 	import Story from "$components/story/story.svelte";
- //	import WIP from "$components/helpers/WIP.svelte";
-	// import Footer from "$components/Footer.svelte";
 
-	// const copy = getContext("copy");
-	// const data = getContext("data");
 </script>
-
-<!-- <WIP />-->
 <Story />
-<!-- <Footer /> -->
+
